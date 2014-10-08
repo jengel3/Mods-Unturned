6be4ad01a949b2d0ac998d79722d04efb6d11f07
Jake <jake0oo0andminecraft@gmail.com>
2014-10-08 18:17:23 -0500

    Remove upload button, sort index page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
   def index
-    @assets = Submission.where(:type => "Asset")
-    @levels = Submission.where(:type => "Level")
+    @assets = Submission.where(:type => "Asset").desc(:created_at)
+    @levels = Submission.where(:type => "Level").desc(:created_at)
   end
 end
