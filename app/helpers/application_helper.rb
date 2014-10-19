4527506824566ce8e60628bbb570395fb7900f2c
Jake <jake0oo0andminecraft@gmail.com>
2014-10-19 14:26:01 -0500

    Redesign progress

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,18 @@ module ApplicationHelper
   def require_admin
     redirect_to root_path unless user_is_admin
   end
+
+  def avatar(user, options = {})
+    image = gravatar_url user.email, options
+    image_tag image, :alt => "Avatar", class: 'avatar' if image.present?
+  end
+
+  def gravatar_url(email, options = {})
+    require 'digest/md5' unless defined?(Digest::MD5)
+    md5 = Digest::MD5.hexdigest(email.to_s.strip.downcase)
+    options[:s] = options.delete(:size) || 60
+    options[:d] = options.delete(:default)
+    options.delete(:d) unless options[:d]
+    "#{request.ssl? ? 'https://secure' : 'http://www'}.gravatar.com/avatar/#{md5}?#{options.to_param}"
+  end
 end
