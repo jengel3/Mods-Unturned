3c51bd4159b4b00dafdbf7b63b942f1160a29759
Jake <jake0oo0andminecraft@gmail.com>
2014-10-25 22:49:54 -0500

    Fix borders

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -3,4 +3,6 @@ $(document).ready(function () {
 	$('.comments').height($('.list').height() + $('.header').height() + 16)
 	$('#mainpage').height($('.comments').height() + 550)
 	$('.infosection').height($('#mainpage').height())
+	$('.mainpageright').height($('#mainpage').height())
+	$('.mainpageleft').height($('#mainpage').height())
 });
