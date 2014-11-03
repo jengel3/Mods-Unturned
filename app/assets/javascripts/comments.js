55fb86e94c107ccc5f85e939471eb716050a65d7
Jake <jake0oo0andminecraft@gmail.com>
2014-11-02 22:20:00 -0600

    Progress

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
-	console.log()
 	$('.comments').height($('.list').height() + $('.header').height() + 16)
 	$('#mainpage').height($('.comments').height() + 550)
-	$('.infosection').height($('#mainpage').height())
+	$('.infosection').height($('#mainpage').height() - 17)
 	$('.mainpageright').height($('#mainpage').height())
 	$('.mainpageleft').height($('#mainpage').height())
 });
