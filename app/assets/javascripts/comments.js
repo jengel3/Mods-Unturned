d88df94579fb7f17e1e4bdb83f4c60e410b0562d
Jake <jake0oo0andminecraft@gmail.com>
2014-12-09 21:16:53 -0600

    Serve Font awesome over SSL

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-	if ($('.mainthumblarge').length) {
-		$('.comments').height($('.list').height() + $('.header').height() + 16);
-		$('#mainpage').height($('.comments').height() + 550);
-		$('.infosection').height($('#mainpage').height());
-		$('.mainpageright').height($('#mainpage').height());
-		$('.mainpageleft').height($('#mainpage').height());
-	}
+  if ($('.mainthumblarge').length) {
+    $('.comments').height($('.list').height() + $('.header').height() + 16);
+    $('#mainpage').height($('.comments').height() + 550);
+    $('.infosection').height($('#mainpage').height());
+    $('.mainpageright').height($('#mainpage').height());
+    $('.mainpageleft').height($('#mainpage').height());
+  }
 });
--- app/assets/javascripts/news.js
+++ app/assets/javascripts/news.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-	if ($('.unturnedupdates').length) {
-		$.getJSON("/api/news",function(result){
-			article = result['appnews']['newsitems'][0];
-			title = article['title'];
-			url = article['url'];
-			content = article['contents'];
-			$.post("/api/tohtml", content, function(result){
-				$(".unturnednewsbubbletextarea").html(result);
-			});
-			$(".unturnednewsbubbleheading").text(title);
-			$(".readmoretext").attr('href', url);
-		});
-	}
+  if ($('.unturnedupdates').length) {
+    $.getJSON("/api/news",function(result){
+      article = result['appnews']['newsitems'][0];
+      title = article['title'];
+      url = article['url'];
+      content = article['contents'];
+      $.post("/api/tohtml", content, function(result){
+        $(".unturnednewsbubbletextarea").html(result);
+      });
+      $(".unturnednewsbubbleheading").text(title);
+      $(".readmoretext").attr('href', url);
+    });
+  }
 });
