2368473ae4d88a813452b94e76442fd0b9c8aee5
Jake <jake0oo0andminecraft@gmail.com>
2014-12-22 22:28:03 -0600

    Serve thumbnails over HTTPS, remove debug

--- app/assets/javascripts/content.js
+++ app/assets/javascripts/content.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
   if ($('.mainthumblarge').length) {
     var height = getPageHeight();
-    console.log(height + ' ayy')
-    //$('.comments').height($('.list').height() + $('.header').height() + 16);
     $('#mainpage').height(height);
     $('.infosection').height(height);
     $('.mainpageright').height(height);
@@ -14,7 +12,5 @@ function getPageHeight() {
     $('.comments').height($('.list').height() + $('.header').height() + 16);
     var right_height = $('.mainscreen').outerHeight(true) + $('.screenshots').outerHeight(true) + $('.comments').outerHeight(true);
     var left_height = $('.sidebar').height();
-    console.log(right_height)
-    console.log(left_height)
     return (right_height >= left_height ? right_height : left_height);
 }
