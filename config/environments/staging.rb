c4fac25c424aae5e7ec054e6a342f8dfc0d44542
Jake <jake0oo0andminecraft@gmail.com>
2014-12-24 12:20:21 -0600

    Use Gmail in staging & test env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -73,4 +73,16 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "localhost:3000",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
