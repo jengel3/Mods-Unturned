022493ff4227d83cde86ec5d224adaadfa155df2
Jake <jake0oo0andminecraft@gmail.com>
2015-01-04 01:49:37 -0600

    Mark comments as deleted, don't actually delete (needs testing

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,18 @@ class Comment
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  default_scope { where(:deleted_at.exists => false).asc(:created_at) }
+
+  before_destroy :mark_deleted
+
+  def mark_deleted
+    self.deleted_at = Time.now
+    self.save!
+    false
+  end
+
   field :text, type: String
+  field :deleted_at, type: String
   belongs_to :submission
   belongs_to :user
 
