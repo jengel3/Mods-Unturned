6206e1f253098e27ed0fa796a33d9455ff09f7fc
Jake <jake0oo0andminecraft@gmail.com>
2015-01-21 19:07:05 -0600

    Add staging to default mongoid yml

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "localhost:3000",
+    domain: "http://mods.jake0oo0.me",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
