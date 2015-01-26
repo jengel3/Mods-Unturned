c89d216705d39359307ad529a6f478773d557639
Jake <jake0oo0andminecraft@gmail.com>
2015-01-25 18:49:31 -0600

    more limit

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -55,7 +55,7 @@ class Submission
         new_popular = Submission.valid.where(:approved_at.gte => Date.today - 24.hours).where(:total_downloads.gte => 20).limit(2)
         veteran = Submission.valid.where(:approved_at.lt => Date.today - 48.hours).where(:total_downloads.gte => 250).limit(2)
         randoms = Submission.valid.in(id: Submission.valid.distinct(:id).sample(2))
-        result = Submission.or(veteran.selector, randoms.selector, new_popular.selector).limit(2)
+        result = Submission.or(veteran.selector, randoms.selector, new_popular.selector).limit(6)
         if !result.any?
           return []
         end
