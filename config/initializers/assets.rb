a06bcd167067bb4b1c11642c54552c376c081705
Jake <jake0oo0andminecraft@gmail.com>
2015-02-06 19:49:12 -0600

    Cache content for longer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( about.css error.css main.css content.css home.css upload.css users.css index.css )
+# Rails.application.config.assets.precompile += %w( about.css error.css main.css content.css home.css upload.css users.css index.css )
+Rails.application.config.assets.precompile += %w( *.js )
+Rails.application.config.assets.precompile += %w( *.css )
