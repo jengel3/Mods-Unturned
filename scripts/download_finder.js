3f3a90b98b1bc34d8d99e35dd0ee46c7f8c58624
Jake <jake0oo0andminecraft@gmail.com>
2015-03-22 11:27:15 -0500

    What causes this :(

--- scripts/download_finder.js
+++ scripts/download_finder.js
@@ -2,3 +2,5 @@ db.downloads.aggregate({ "$group": { "_id": "$ip", "count" : { "$sum" : 1 } }},{
 
 
 db.downloads.aggregate({ "$match" : { submission_id: new ObjectId("54c64d4576707326e51b0000") } }, { "$group": { "_id": "$ip", "count" : { "$sum" : 1 } }},{ "$sort" : { count: -1 } })
+
+db.downloads.aggregate({ "$match" : { "created_at" : {"$gte": new Date(Date.UTC(2015, 2, 1)) } } }, { "$group": { "_id": "$ip", "count" : { "$sum" : 1 } }},{ "$sort" : { count: -1 } })
