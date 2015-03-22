f9f61fa7b1ac98efeca1ac8c60b55a68fd0d8c2c
Jake <jake0oo0andminecraft@gmail.com>
2015-03-22 11:38:47 -0500

    Fix stats rendering wrong, and move to stats page

--- app/assets/javascripts/admin/reports.js
+++ app/assets/javascripts/admin/reports.js
@@ -1,5 +1,4 @@
 if (typeof String.prototype.startsWith != 'function') {
-  // see below for better implementation!
   String.prototype.startsWith = function (str){
     return this.indexOf(str) == 0;
   };
