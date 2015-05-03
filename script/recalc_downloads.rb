f5e2c3fc4931347f3b50e96eef2613109b028351
Jake <jake0oo0dev@gmail.com>
2015-05-03 18:40:58 -0500

    Update calc script

--- script/recalc_downloads.rb
+++ script/recalc_downloads.rb
@@ -1,4 +1,8 @@
 Submission.each do |submission|
-  submission.total_downloads = submission.downloads.count
+  original = submission.total_downloads
+  new_count = submission.downloads.count
+  submission.total_downloads = new_count
   submission.save
+  change = new_count - original
+  puts "Download count change: #{change}"
 end
