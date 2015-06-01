cf03085067acfd663e81725a854151c1e405eb84
Jake <jake0oo0dev@gmail.com>
2015-06-01 12:44:28 -0500

    Cleanup random files

--- spec/controllers/admin/control_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Admin::ControlController, :type => :controller do
-
-end
--- spec/factories/reports.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :report do
-    report_type "MyString"
-comments "MyString"
-  end
-
-end
